87347a4bb26a973d506ec100e4f36ea0b1566794
Carl Paton <cpaton@trademe.local>
2018-10-18 14:59:20 +1300

    initial

---
+++ PlayMeFileExtensionUpdater/ProcessDirectory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace PlayMeFileExtensionUpdater
+{
+    internal class ProcessDirectory
+    {
+        private readonly string newFileExtension;
+        private readonly string pathToUpdate;
+
+        public ProcessDirectory(string newFileExtension, string pathToUpdate)
+        {
+            this.newFileExtension = newFileExtension;
+            this.pathToUpdate = pathToUpdate;
+        }
+
+        public void Go()
+        {
+            if (string.IsNullOrEmpty(newFileExtension) || string.IsNullOrEmpty(pathToUpdate))
+                return;
+
+            foreach (var path in Directory.EnumerateDirectories(pathToUpdate))
+            {
+                foreach (var file in Directory.GetFiles(path)) //what about sub-dirs my bru?
+                {
+                    var fi = new FileInfo(file);
+                    if (!fi.Extension.Equals(".pme"))
+                    {
+                        fi.MoveTo(Path.Combine(path, fi.Name.Replace(fi.Extension, "") + "." + newFileExtension));
+                        Console.WriteLine(file);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ PlayMeFileExtensionUpdater/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PlayMeFileExtensionUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pop this into a json config file you lazy bastard
+            var newFileExtension = "pme";
+            var pathToUpdate = @"C:\Data\Carl\pluralsight\angular-2-getting-started-update\";
+
+            new ProcessDirectory(newFileExtension, pathToUpdate)
+                .Go();
+
+            Console.WriteLine("Hello New Zealand!");
+        }
+    }
+}
